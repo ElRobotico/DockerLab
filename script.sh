25e4ef8fb22a9d39261020195713f4dfda90b458
Reda2200 <119880230+Reda2200@users.noreply.github.com>
2023-05-29 10:14:08 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo apt update 
+sudo apt upgrade -y 
+#docker
+sudo apt install ca-certificates curl gnupg lsb-release 
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list 
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
